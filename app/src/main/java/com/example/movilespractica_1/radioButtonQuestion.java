0b22937d0089c96137e13c50540a246c761365df
Javiex <javiexalonso@gmail.com>
2021-10-17 03:02:26 +0200

    Fragment Button

---
+++ app/src/main/java/com/example/movilespractica_1/radioButtonQuestion.java
@@ -0,0 +1,30 @@
+package com.example.movilespractica_1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class radioButtonQuestion extends Fragment {
+
+    public radioButtonQuestion() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_radio_button_question, container, false);
+    }
+}
